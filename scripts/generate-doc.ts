f91712852446c49bf4613d777a9d8e9d5a4b6310
paldepind <simonfv@gmail.com>
2018-03-24 14:56:55 +0100

    Create default theme

--- scripts/generate-doc.ts
+++ scripts/generate-doc.ts
@@ -1,26 +1,40 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import {generateJSON} from '../src/index'
-import * as pug from 'pug'
+// import * as pug from 'pug'
+import * as handlebars from 'handlebars'
 import * as yargs from 'yargs'
+import * as sass from 'node-sass'
 
 function generateDocumentation(argv) {
-  const template = path.resolve(__dirname, '../templates/index.pug')
+  // const template = path.resolve(__dirname, '../templates/index.pug')
+  const styleFile = path.resolve(__dirname, '../templates/style.scss')
 
-  const templateFn = pug.compileFile(template)
+  // const templateFn = pug.compileFile(template)
 
   const source = argv._[0]
+
+  // Generate data and data.json
   const data = generateJSON(source)
+  fs.writeFileSync(path.resolve(argv.o, 'data.json'), JSON.stringify(data))
 
-  const rendered = templateFn({data})
+  // Generate index.html
+  const template = fs.readFileSync(
+    path.resolve(__dirname, '../templates/index.handlebars'),
+    'utf8'
+  )
+  const rendered = handlebars.compile(template)({data})
+  fs.writeFileSync(path.resolve(argv.o, 'index.html'), rendered)
 
-  fs.writeFileSync('data.json', JSON.stringify(data))
-  fs.writeFileSync('index.html', rendered)
+  // Generate styleFile
+  const sassResult = sass.renderSync({file: styleFile})
+  fs.writeFileSync(path.resolve(argv.o, 'style.css'), sassResult.css)
 }
 
 const argv = yargs
   .alias('o', 'output')
-  .describe('o', 'destination for output')
+  .describe('o', 'directory for output')
+  .default('o', 'dist')
   .help('help').argv
 
 generateDocumentation(argv)
